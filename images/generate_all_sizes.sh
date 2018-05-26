e4662876caeebb3f2ef508da4a579782c5c09472
may <mehrabhoque@gmail.com>
2018-05-26 19:00:39 -0400

    Fix green-line anti-aliasing artifacts and ...
    also add a script for generatizing the resized icons quicker than doing
    it by hand in gimp.

---
+++ images/generate_all_sizes.sh
@@ -0,0 +1,3 @@
+convert -define png:format=png32 com128.png -resize 48x48 com48.png
+convert -define png:format=png32 com128.png -resize 32x32 com32.png
+convert -define png:format=png32 com128.png -resize 16x16 com16.png
