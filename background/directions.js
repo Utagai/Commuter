daa614cd9b84a2784b8dfaa76f6939757cd5ecbf
may <mehrabhoque@gmail.com>
2018-05-26 17:26:05 -0400

    Add support for both latlng coordinate based directions and string.
    And also add some partial implementation for listings page based
    directions finding.

--- background/directions.js
+++ background/directions.js
@@ -1,13 +1,52 @@
 'use strict';
 
-function getDirectionsToOfficeFrom(latlng, sendResponse) {
-  console.log("Mongo latlng: " + mongoLatLng);
+function getDirectionsMultiple(latlngs, sendResponse) {
+  let latlngsResults = {}
+  function saveResult(result) {
+    console.log("Saving result for latlng: " + JSON.stringify(result.origin));
+    latlngsResults[result.origin] = result.duration;
+  }
+
+  latlngs.forEach(function(latlng) {
+    console.log("Computing directions for latlng: " + JSON.stringify(latlng));
+    getDirectionsCoords(latlng, saveResult);
+  });
+
+  function waitForDirections() {
+    if (Object.keys(latlngsResults).length != latlngs.length) {
+      console.log("Waiting again...");
+      setTimeout(waitForDirections, 100);
+    } else {
+      console.log("All directions computed: ");
+      console.log(latlngsResults);
+      return;
+    }
+  }
+
+  waitForDirections();
+}
+
+function getDirectionsString(address, sendResponse) {
+  var request = {
+    origin: address,
+    destination: new google.maps.LatLng(mongoLatLng.lat, mongoLatLng.lng),
+    travelMode: 'TRANSIT'
+  };
+
+  getDirections(request, sendResponse);
+}
+
+function getDirectionsCoords(latlng, sendResponse) {
   var request = {
     origin: new google.maps.LatLng(latlng.lat, latlng.lng),
     destination: new google.maps.LatLng(mongoLatLng.lat, mongoLatLng.lng),
     travelMode: 'TRANSIT'
   };
 
+  getDirections(request, sendResponse);
+}
+
+function getDirections(request, sendResponse) {
   directions.route(request, function(result, status) {
     if (status == 'OK') {
       console.log("Got a response from directions API:");
@@ -15,6 +54,8 @@ function getDirectionsToOfficeFrom(latlng, sendResponse) {
       directionsResult = result;
       let directionsResp = {
         'duration': result.routes[0].legs[0].duration.text,
+        'origin': request.origin,
+        'dest': mongoLatLng,
         'gmapsRes': result
       };
 
