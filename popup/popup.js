23e42fdc5632786f8c9349a04c0881291dff1f49
may <mehrabhoque@gmail.com>
2018-05-26 03:21:37 -0400

    Get prettier path drawing in gmaps popup.

--- popup/popup.js
+++ popup/popup.js
@@ -7,6 +7,33 @@ var map;
 
 var mongoIconUrl = "https://static.filehorse.com/icons/"
   + "developer-tools/mongodb-icon-32.png";
+var kNoPathZoom = 15;
+var kPathZoom = 13;
+
+function avg(nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+
+  return sum/nums.length;
+}
+
+function getAvgLatLng(coords) {
+  let lats = coords.map(function(coord) {
+    return coord.lat;
+  });
+
+  let lngs = coords.map(function(coord) {
+    return coord.lng;
+  });
+
+  console.log("Lats: " + JSON.stringify(lats));
+  console.log("Lngs: " + lngs);
+
+  return new google.maps.LatLng(avg(lats), avg(lngs));
+  //return { 'lat': avg(lats), 'lng': avg(lngs) };
+}
 
 function potentiallyUpdateMap() {
   console.log("Popup clicked!");
@@ -16,7 +43,19 @@ function potentiallyUpdateMap() {
     },
     function(response) {
       console.log(response);
-      directionsDisplay.setDirections(response);
+      let directionsCoords = response.routes[0].overview_path;
+      console.log(directionsCoords);
+      let directionsPath = new google.maps.Polyline({
+          path: directionsCoords,
+          geodesic: true,
+          strokeColor: '#ffb5cc',
+          strokeOpacity: 0.7,
+          strokeWeight: 5
+      });
+      let avgLatLng = getAvgLatLng(directionsCoords);
+      map.setCenter(avgLatLng);
+      map.setZoom(kPathZoom);
+      directionsPath.setMap(map);
     }
   );
 }
@@ -27,7 +66,7 @@ function initGmaps() {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: mongoLatLng,
-    zoom: 15
+    zoom: kNoPathZoom
   });
 
   let marker = new google.maps.Marker(
