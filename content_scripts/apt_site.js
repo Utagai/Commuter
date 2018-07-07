0eabbaedac8fb83e198c18eded4b81b58e3b4fb2
may <mehrabhoque@gmail.com>
2018-07-07 02:55:26 -0400

    Refactor content script code into extendible OOP
    Refactored the content script code to be extendable and structured. The
    idea is now that if we wish to support another site (like
    apartments.com) then we implement the ApartmentSite interface and
    implement the necessary functions. Upon doing so, the rest of the code
    follows the exact same logical sequence removing duplication. This
    interface gives us structure by emphasizing on the similarities between
    sites and letting the site-specific logic involving things like their
    HTML isolated to each site and hidden from others.

---
+++ content_scripts/apt_site.js
@@ -0,0 +1,145 @@
+/* eslint-disable no-unused-vars */
+/**
+ * An interface that must be implemented for each apartment site that is
+ * needed to be supported. (e.g. StreetEasy inherits this, Zillow, etc).
+ *
+ * This interface defines two functions for injecting commute times into pages,
+ * as the logic for doing so will depend on the HTML layout of the respective
+ * websites.
+ *
+ * If support for <apartment_site>.com is wished to be added, this interface
+ * must be inherited and its functions be implemented. If they are not, they
+ * will throw FunctionNotDefined errors. The functions should be implemented so
+ * that given a response containing the commute time(s) of the listing(s),
+ * these times are nicely injected into the site's content.
+ */
+class ApartmentSite {
+/* eslint-disable no-unused-vars */
+
+  /**
+   * The constructor for this ApartmentSite object.
+   *
+   * Simply records the name of the site.
+   *
+   * @param {string} siteName The name of the apartment site, as a string. This
+   *  is not the url.
+   */
+  constructor(siteName) {
+    this.siteName = siteName;
+  }
+
+  /**
+   * Extracts the human-readable address string of a listing (singular) page.
+   *
+   * @throws {FunctionNotDefined} This function will throw a FunctionNotDefined
+   * error if it is called directly as a function of ApartmentSite or by one of
+   * its children classes if they have not overridden this function with their
+   * own implementation.
+   */
+  extractListingAddress() {
+    throw FunctionNotDefined;
+  }
+
+  /**
+   * Extracts the human-readable listing addresses from a listings (plural)
+   * page.
+   *
+   * @throws {FunctionNotDefined} This function will throw a FunctionNotDefined
+   * error if it is called directly as a function of ApartmentSite or by one of
+   * its children classes if they have not overridden this function with their
+   * own implementation.
+   */
+  extractListingAddresses() {
+    throw FunctionNotDefined;
+  }
+
+  /**
+   * Injects commute times into the apartment site's listing page.
+   *
+   * @param {object} response The result response from which to get the
+   *  duration.
+   * @throws {FunctionNotDefined} This function will throw a FunctionNotDefined
+   * error if it is called directly as a function of ApartmentSite or by one of
+   * its children classes if they have not overridden this function with their
+   * own implementation.
+   */
+  injectCommuteTime(response) {
+    throw FunctionNotDefined;
+  }
+
+  /**
+   * Injects multiple commute times into the apartment site's listings (plural)
+   * page.
+   *
+   * See injectCommuteTime(response) for more information.
+   *
+   * @param {object} response The response containing the durations for all the
+   *  listings in the page.
+   * @throws {FunctionNotDefined} This function will throw a FunctionNotDefined
+   * error if it is called directly as a function of ApartmentSite or by one of
+   * its children classes if they have not overridden this function with their
+   * own implementation.
+   */
+  injectCommuteTimes(response) {
+    throw FunctionNotDefined;
+  }
+
+  /**
+   * An entry point function that is executed once the URL for this apartment
+   * site is visited and triggers the execution of the logic which would
+   * ultimately be responsible for injecting commute times into the page.
+   *
+   * This function should essentially be the entry point for the respective
+   * content script for the apartment site that this object represents.
+   */
+  run() {
+    let listingAddress = this.extractListingAddress();
+    if (listingAddress) {
+      console.log('Found a listing page!');
+      reportListingPage(listingAddress, this);
+    } else {
+      let listingAddresses = this.extractListingAddresses();
+      console.log(listingAddresses);
+      if (listingAddresses.length > 0) {
+        reportListingsPage(listingAddresses, this);
+      } else {
+        console.log('Not a listing or listings page.');
+      }
+    }
+  }
+}
+
+/**
+ * Reports the extracted information gleaned from the listing (singular) page.
+ *
+ * @param {string} listingAddress The address string of the listing.
+ * @param {string} source Denotes the source apartment site of this message.
+ */
+function reportListingPage(listingAddress, source) {
+  chrome.runtime.sendMessage(
+    {
+      'source': source.siteName,
+      'type': 'listing',
+      'addressInfo': listingAddress,
+    },
+    source.injectCommuteTime
+  );
+}
+
+/**
+ * Reports the extracted information gleaned from the listings (plural) page.
+ *
+ * @param {array} listingAddresses An array of all listing addresses found on
+ *  listings page.
+ * @param {string} source Denotes the source apartment site of this message.
+ */
+function reportListingsPage(listingAddresses, source) {
+  chrome.runtime.sendMessage(
+    {
+      'source': source.siteName,
+      'type': 'listings',
+      'addressInfo': listingAddresses,
+    },
+    source.injectCommuteTimes
+  );
+}
--- content_scripts/streeteasy.js
+++ content_scripts/streeteasy.js
@@ -4,168 +4,135 @@
  * @author may
  */
 
+/* eslint-disable no-unused-vars */
 /**
- * Injects commute times into the StreetEasy listing page.
- *
- * @param {object} response The result response from which to get the duration.
+ * Implements the ApartmentSite interface with StreetEasy specific
+ * functionality.
  */
-function injectCommuteTime(response) {
-  if (response) {
-    console.log(response);
-    let duration = response.duration;
-    let titleElems = document.getElementsByClassName('incognito');
-    for (let i = 0; i < titleElems.length; i++) {
-      titleElems[i].innerText = titleElems[i].innerText + ' (' + duration + ')';
+class StreetEasy extends ApartmentSite {
+/* eslint-enable no-unused-vars */
+  /* eslint-disable no-unused-vars */
+  /**
+   * Injects commute times into the StreetEasy listing page.
+   *
+   * @param {object} response The result response from which to get the
+   *  duration.
+   */
+  injectCommuteTime(response) {
+  /* eslint-enable no-unused-vars */
+    if (response) {
+      console.log(response);
+      let duration = response.duration;
+      let titleElems = document.getElementsByClassName('incognito');
+      for (let i = 0; i < titleElems.length; i++) {
+        titleElems[i].innerText = titleElems[i].innerText
+          + ' (' + duration + ')';
+      }
     }
   }
-}
 
-/**
- * Finds and returns the title tag corresponding to the listing title from the
- * page.
- *
- * @param {string} articleTitle The HTML element referring to the article
- *  element containing the title.
- * @return {HTMLElement} found title tag.
- */
-function findTitleTag(articleTitle) {
-  let titleDetails = Array.from(articleTitle.childNodes).filter(
-    function(child) {
-      return child.nodeName.toLowerCase() == 'h3';
-    }
-  )[0];
+  /**
+   * Finds and returns the title tag corresponding to the listing title from the
+   * page.
+   *
+   * @param {string} articleTitle The HTML element referring to the article
+   *  element containing the title.
+   * @return {HTMLElement} found title tag.
+   */
+  static findTitleTag(articleTitle) {
+    let titleDetails = Array.from(articleTitle.childNodes).filter(
+      function(child) {
+        return child.nodeName.toLowerCase() == 'h3';
+      }
+    )[0];
 
-  return Array.from(titleDetails.childNodes).filter(function(child) {
-    return child.nodeName.toLowerCase() == 'a';
-  })[0];
-}
-
-/**
- * Injects multiple commute times into the StreetEasy listings (plural) page.
- *
- * See injectCommuteTime(response) for more information.
- *
- * @param {object} response The response containing the durations for all the
- *  listings in the page.
- */
-function injectCommuteTimes(response) {
-  if (response) {
-    console.log(response);
-    let durations = response.durations;
-    let articleTitles = extractArticleTitles();
-    for (let i = 0; i < durations.length; i++) {
-      let titleTag = findTitleTag(articleTitles[i]);
-      titleTag.innerText = '(' + durations[i] + ') ' + titleTag.innerText;
-      console.log('Title tag:');
-      console.log(titleTag);
-    }
-  }
-}
-
-/**
- * Extracts the human-readable string address of a listing from the page.
- *
- * @return {string} A string representing the address of the listing.
- */
-function extractListingAddress() {
-  let infos = document.getElementsByClassName('backend_data BuildingInfo-item');
-  if (infos.length == 0) {
-    return null;
-  }
-  let listingInfo = infos[0];
-  let listingAddress = listingInfo.innerText;
-  console.log('Listing info found: ' + listingAddress);
-  return listingAddress;
-}
-
-/**
- * Extracts the article titles from the page and returns them.
- *
- * This runs for a listings (plural) page, and therefore finds all the articles
- * with address information in them.
- *
- * @return {HTMLElement} The article element.
- */
-function extractArticleTitles() {
-  let articles = Array.from(document.getElementsByTagName('article'));
-  return articles.map(function(article) {
-    let articleChildren = Array.from(article.childNodes);
-    return articleChildren.filter(function(child) {
-      return child.className == 'details row';
+    return Array.from(titleDetails.childNodes).filter(function(child) {
+      return child.nodeName.toLowerCase() == 'a';
     })[0];
-  });
-}
-
-/**
- * Extracts the listing addresses from a series of article titles.
- *
- * @return {array} A series of listing addresses that were extracted.
- */
-function extractListingAddresses() {
-  let listingsContainer = document.getElementsByClassName('left-two-thirds '
-    + 'items item-rows listings');
-  if (listingsContainer.length == 0) {
-    return [];
   }
-  return extractArticleTitles().map(function(articleTitle) {
-    return findTitleTag(articleTitle).innerText + ', ' + destState;
-  });
-}
 
-/**
- * Reports the extracted information gleaned from the listing (singular) page.
- *
- * @param {string} listingAddress The address string of the listing.
- * @param {function} callback Call back function to call once the directions for
- *  the given listingAddress has been computed.
- */
-function reportListingPage(listingAddress, callback) {
-  chrome.runtime.sendMessage(
-    {
-      'source': 'streateasy',
-      'type': 'listing',
-      'addressInfo': listingAddress,
-    },
-    callback
-  );
-}
-
-/**
- * Reports the extracted information gleaned from the listings (plural) page.
- *
- * @param {array} listingAddresses An array of all listing addresses found on
- *  listings page.
- * @param {function} callback The callback function to call once the directions
- *  for the various listing addresses have been computed.
- */
-function reportListingsPage(listingAddresses, callback) {
-  chrome.runtime.sendMessage(
-    {
-      'source': 'streateasy',
-      'type': 'listings',
-      'addressInfo': listingAddresses,
-    },
-    callback
-  );
-}
-
-/**
- * The entry point for this content-script to run on StreetEasy URLs.
- */
-function run() {
-  let listingAddress = extractListingAddress();
-  if (listingAddress) {
-    console.log('Found a listing page!');
-    reportListingPage(listingAddress, injectCommuteTime);
-  } else {
-    let listingAddresses = extractListingAddresses();
-    console.log(listingAddresses);
-    if (listingAddresses.length > 0) {
-      reportListingsPage(listingAddresses, injectCommuteTimes);
-    } else {
-      console.log('Not a listing or listings page.');
+  /* eslint-disable no-unused-vars */
+  /**
+   * Injects multiple commute times into the StreetEasy listings (plural) page.
+   *
+   * See injectCommuteTime(response) for more information.
+   *
+   * @param {object} response The response containing the durations for all the
+   *  listings in the page.
+   */
+  injectCommuteTimes(response) {
+  /* eslint-enable no-unused-vars */
+    if (response) {
+      console.log(response);
+      let durations = response.durations;
+      let articleTitles = StreetEasy.extractArticleTitles();
+      for (let i = 0; i < durations.length; i++) {
+        let titleTag = StreetEasy.findTitleTag(articleTitles[i]);
+        titleTag.innerText = '(' + durations[i] + ') ' + titleTag.innerText;
+        console.log('Title tag:');
+        console.log(titleTag);
+      }
     }
   }
+
+  /* eslint-disable no-unused-vars */
+  /**
+   * Extracts the human-readable address string of a listing from the page.
+   *
+   * @return {string} A string representing the address of the listing.
+   */
+  extractListingAddress() {
+    /* eslint-enable no-unused-vars */
+    let infos = document
+      .getElementsByClassName('backend_data BuildingInfo-item');
+    if (infos.length == 0) {
+      return null;
+    }
+    let listingInfo = infos[0];
+    let listingAddress = listingInfo.innerText;
+    console.log('Listing info found: ' + listingAddress);
+    return listingAddress;
+  }
+
+  /* eslint-disable no-unused-vars */
+  /**
+   * Extracts the article titles from the page and returns them.
+   *
+   * This runs for a listings (plural) page, and therefore finds all the
+   * articles with address information in them.
+   *
+   * @return {HTMLElement} The article element.
+   */
+  static extractArticleTitles() {
+    /* eslint-enable no-unused-vars */
+    let articles = Array.from(document.getElementsByTagName('article'));
+    return articles.map(function(article) {
+      let articleChildren = Array.from(article.childNodes);
+      return articleChildren.filter(function(child) {
+        return child.className == 'details row';
+      })[0];
+    });
+  }
+
+  /* eslint-disable no-unused-vars */
+  /**
+   * Extracts the human-readable listing addresses from a series of article
+   * titles.
+   *
+   * @return {array} A series of listing addresses that were extracted.
+   */
+  extractListingAddresses() {
+    /* eslint-enable no-unused-vars */
+    let listingsContainer = document.getElementsByClassName('left-two-thirds '
+      + 'items item-rows listings');
+    if (listingsContainer.length == 0) {
+      return [];
+    }
+    return StreetEasy.extractArticleTitles().map((articleTitle) => {
+      return StreetEasy.findTitleTag(articleTitle).innerText + ', ' + destState;
+    });
+  }
 }
 
-run();
+streetEasy = new StreetEasy('streeteasy');
+streetEasy.run();
