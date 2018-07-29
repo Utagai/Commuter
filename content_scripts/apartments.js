7af0b79512717dc97d0111ab22c9763ba72b67c2
may <mehrabhoque@gmail.com>
2018-07-29 17:13:25 -0400

    Implement extractListingAddress() for apartments.js
    Title is self-explanatory, again.

--- content_scripts/apartments.js
+++ content_scripts/apartments.js
@@ -14,3 +14,34 @@ function extractListingAddresses() {
     return location.innerText;
   });
 }
+
+/**
+ * Cleans a given raw address by removing its neighborhood designation and
+ * stripping the string of any beginning and trailing whitespace.
+ *
+ * @param {string} rawAddress The raw address string to clean.
+ * @return {string} The cleaned version of the given raw address.
+ */
+function cleanAddress(rawAddress) {
+  return rawAddress.substring(0, rawAddress.indexOf('–')).trim();
+}
+
+/**
+ * Extracts the human-readable address string of a listing from the page.
+ *
+ * @return {string} A string representing the address of the listing.
+ */
+function extractListingAddress() {
+  let propertyAddressElems = document.getElementsByClassName(
+    'propertyAddressRow'
+  );
+  let properyAddressElem = propertyAddressElems[0];
+  if (typeof propertyAddressElem === 'undefined') {
+    console.log('Did not find the property address element.');
+    return;
+  }
+  let addressRaw = properyAddressElem.innerText;
+  let address = cleanAddress(addressRaw);
+  console.log('Address of the current unit: ' + address);
+  return address;
+}
