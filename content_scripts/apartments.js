3323d7d1f664594a38a5b4c19722e4e71615e339
may <mehrabhoque@gmail.com>
2018-07-29 17:11:59 -0400

    Implement extractListingAddresses() for apartments.js
    Title is self-explanatory.

--- content_scripts/apartments.js
+++ content_scripts/apartments.js
@@ -1,2 +1,16 @@
-console.log('Apartments js loaded!');
-console.log('Ayyyooooo');
+/**
+ * Extracts the human-readable listing addresses from a series of article
+ * titles.
+ *
+ * @return {array} A series of listing addresses that were extracted.
+ */
+function extractListingAddresses() {
+  let locationClassElems = Array.from(
+    document.getElementsByClassName('location')
+  );
+  let locationElems = locationClassElems.splice(1);
+  return locationElems.map((location) => {
+    console.log('Location inner text: "' + location.innerText + '"');
+    return location.innerText;
+  });
+}
