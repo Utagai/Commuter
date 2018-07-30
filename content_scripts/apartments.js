3686f552609d50fe39dba86b1539724f7c4884e2
may <mehrabhoque@gmail.com>
2018-07-30 15:01:49 -0400

    Incorporate apartments.js logic into commuter core
    Subclass apartments.js under ApartmentSite and incorporate the logic
    into the backend core for calculating commute times. Apartments.com
    basically works now, but since it displays more results than StreetEasy
    we will probably need to increase the delay between requests... I guess
    this is our punishment for not wanting to pay.

--- content_scripts/apartments.js
+++ content_scripts/apartments.js
@@ -1,95 +1,102 @@
 /**
- * Extracts the human-readable listing addresses from a series of article
- * titles.
+ * Loaded when any Apartments.com URL is visited.
  *
- * @return {array} A series of listing addresses that were extracted.
+ * @author may
  */
-function extractListingAddresses() {
-  let locationClassElems = Array.from(
-    document.getElementsByClassName('location')
-  );
-  let locationElems = locationClassElems.splice(1);
-  return locationElems.map((location) => {
-    console.log('Location inner text: "' + location.innerText + '"');
-    return location.innerText;
-  });
-}
 
 /**
- * Cleans a given raw address by removing its neighborhood designation and
- * stripping the string of any beginning and trailing whitespace.
- *
- * @param {string} rawAddress The raw address string to clean.
- * @return {string} The cleaned version of the given raw address.
+ * Implements the ApartmentSite interface with StreetEasy specific
+ * functionality.
  */
-function cleanAddress(rawAddress) {
-  return rawAddress.substring(0, rawAddress.indexOf('–')).trim();
-}
-
-/**
- * Extracts the human-readable address string of a listing from the page.
- *
- * @return {string} A string representing the address of the listing.
- */
-function extractListingAddress() {
-  let propertyAddressElems = document.getElementsByClassName(
-    'propertyAddressRow'
-  );
-  let properyAddressElem = propertyAddressElems[0];
-  if (typeof propertyAddressElem === 'undefined') {
-    console.log('Did not find the property address element.');
-    return;
+class Apartments extends ApartmentSite {
+  /**
+   * Extracts the human-readable listing addresses from a series of article
+   * titles.
+   *
+   * @return {array} A series of listing addresses that were extracted.
+   */
+  extractListingAddresses() {
+    let locationClassElems = Array.from(
+      document.getElementsByClassName('location')
+    );
+    let locationElems = locationClassElems.splice(1);
+    return locationElems.map((location) => {
+      console.log('Location inner text: "' + location.innerText + '"');
+      return location.innerText;
+    });
   }
-  let addressRaw = properyAddressElem.innerText;
-  let address = cleanAddress(addressRaw);
-  console.log('Address of the current unit: ' + address);
-  return address;
-}
 
-/**
- * Injects multiple commute times into the StreetEasy listings (plural) page.
- *
- * See injectCommuteTime(response) for more information.
- *
- * @param {object} response The response containing the durations for all the
- *  listings in the page.
- */
-function injectCommuteTimes(response) {
-  if (response) {
-    console.log('Got response:');
-    console.log(response);
-    let durations = response.durations;
-    let titleElems = document.getElementsByClassName('placardTitle');
-    for (let i = 0; i < durations.length; i++) {
-      titleElems[i].innerText += createDurationSuffix(durations[i]);
+  /**
+   * Cleans a given raw address by removing its neighborhood designation and
+   * stripping the string of any beginning and trailing whitespace.
+   *
+   * @param {string} rawAddress The raw address string to clean.
+   * @return {string} The cleaned version of the given raw address.
+   */
+  static cleanAddress(rawAddress) {
+    return rawAddress.substring(0, rawAddress.indexOf('–')).trim();
+  }
+
+  /**
+   * Extracts the human-readable address string of a listing from the page.
+   *
+   * @return {string} A string representing the address of the listing.
+   */
+  extractListingAddress() {
+    let propertyAddressElems = document.getElementsByClassName(
+      'propertyAddressRow'
+    );
+    console.log('Found property address elems:');
+    console.log(propertyAddressElems);
+    let propertyAddressElem = propertyAddressElems[0];
+    console.log('Found property address elem:');
+    console.log(propertyAddressElem);
+    if (typeof propertyAddressElem === 'undefined') {
+      console.log('Did not find the property address element.');
+      return;
+    }
+    let addressRaw = propertyAddressElem.innerText;
+    let address = Apartments.cleanAddress(addressRaw);
+    console.log('Address of the current unit: ' + address);
+    return address;
+  }
+
+  /**
+   * Injects multiple commute times into the StreetEasy listings (plural) page.
+   *
+   * See injectCommuteTime(response) for more information.
+   *
+   * @param {object} response The response containing the durations for all the
+   *  listings in the page.
+   */
+  injectCommuteTimes(response) {
+    if (response) {
+      console.log('Got response:');
+      console.log(response);
+      let durations = response.durations;
+      let titleElems = document.getElementsByClassName('placardTitle');
+      for (let i = 0; i < durations.length; i++) {
+        titleElems[i].innerText += createDurationSuffix(durations[i]);
+      }
+    }
+  }
+
+  /**
+   * Injects commute times into the StreetEasy listing page.
+   *
+   * @param {object} response The result response from which to get the
+   *  duration.
+   */
+  injectCommuteTime(response) {
+    if (response) {
+      console.log('Got response:');
+      console.log(response);
+      let duration = response.duration;
+      let listingTitle = document.getElementsByClassName('propertyName')[0];
+      listingTitle.innerText += createDurationSuffix(duration);
     }
   }
 }
 
-/**
- * Injects commute times into the StreetEasy listing page.
- *
- * @param {object} response The result response from which to get the
- *  duration.
- */
-function injectCommuteTime(response) {
-  if (response) {
-    console.log('Got response:');
-    console.log(response);
-    let duration = response.duration;
-    let listingTitle = document.getElementsByClassName('propertyName')[0];
-    listingTitle.innerText += createDurationSuffix(duration);
-  }
-}
-
-extractListingAddresses();
-injectCommuteTimes(function() {
-  let numDurations = 25;
-  durations = [];
-  for (let i = 0; i < numDurations; i++) {
-    durations.push(Math.floor(Math.random() * Math.floor(60)) + ' mins');
-  }
-  return {'durations': durations};
-}());
-extractListingAddress();
-injectCommuteTime({'duration': '43 min'});
+apartments = new Apartments('apartments');
+apartments.run();
